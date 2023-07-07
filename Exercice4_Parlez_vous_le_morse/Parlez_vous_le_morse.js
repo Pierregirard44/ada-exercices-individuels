374d7ef27506ed5ebc553c916b6317df825d3499
root <root@LAPTOP-I4U4E0I4>
2023-07-07 10:34:20 +0200

    ajout exo4

---
+++ Exercice4_Parlez_vous_le_morse/Parlez_vous_le_morse.js
@@ -0,0 +1,6 @@
+let numberCaptain = 0
+function giveNumber() {
+    var numberDroid =document.getElementById("captain").value;
+    alert("Vous avez saisi : "+ numberDroid + " droïdes à trouver");
+    alert("Passez la main au Stormtrooper qui doit trouver la réponse.")
+}
